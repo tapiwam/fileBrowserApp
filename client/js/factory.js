bc9fb46c02cf7a72838cebb8090a3d7a653b562b
Tapiwa M <tapiwa.maruni@gmail.com>
2015-06-07 18:13:24 -0500

    Initial commit

---
+++ client/js/factory.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+ 
+  app.factory('FetchFileFactory', ['$http',
+    function($http) {
+      var _factory = {};
+ 
+      _factory.fetchFile = function(file) {
+        return $http.get('/api/resource?resource=' + encodeURIComponent(file));
+      };
+ 
+      return _factory;
+    }
+  ]);
+ 
+}());
